0cd2a08627684e2cef93a404b39a4801a362cbe7
Han <zhonghanjie@flyele.net>
2023-03-09 18:03:42 +0800

    feat: tslint

--- apps/web/src/app/app.tsx
+++ apps/web/src/app/app.tsx
@@ -1,13 +1,13 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import styles from './app.module.scss'
 import NxWelcome from './nx-welcome'
-import { Banner } from '@flyele-nx/service-module'
+// import { Banner } from '@flyele-nx/service-module'
 
 export function App() {
   return (
     <>
       <NxWelcome title="web" />
-      <Banner />
+      {/* <Banner /> */}
       <div />
     </>
   )

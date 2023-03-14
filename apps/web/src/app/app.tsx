91493bb7e92a337dc4649d0199d3af05f12badce
Han <zhonghanjie@flyele.net>
2023-03-14 18:00:35 +0800

    chore: imgType for luraModal

--- apps/web/src/app/app.tsx
+++ apps/web/src/app/app.tsx
@@ -4,7 +4,17 @@ export function App() {
   return (
     <>
       <div />
-      {/* <LureModal open={true} handleClose={() => {}} handleConfirm={() => {}} /> */}
+      {/* <LureModal
+        open={true}
+        handleClose={() => {}}
+        handleConfirm={() => {}}
+        title="增加文sd件大小"
+        tip="tipasdasdas"
+        freeTxt="30M"
+        vipTxt="50M"
+        highlight="在思维导图中结构化创建事项"
+        imgType="application"
+      /> */}
     </>
   )
 }

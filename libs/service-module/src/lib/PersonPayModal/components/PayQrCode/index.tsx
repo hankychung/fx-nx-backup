ac287fe6649b7e38ea0da5756a23649a2e10c95a
wanghui <wanghui@flyele.net>
2023-03-17 11:57:12 +0800

    feat: 支付码

--- libs/service-module/src/lib/PersonPayModal/components/PayQrCode/index.tsx
+++ libs/service-module/src/lib/PersonPayModal/components/PayQrCode/index.tsx
@@ -2,25 +2,52 @@
  * @Author: wanghui wanghui@flyele.net
  * @Date: 2023-03-09 09:55:49
  * @LastEditors: wanghui wanghui@flyele.net
- * @LastEditTime: 2023-03-13 10:00:52
+ * @LastEditTime: 2023-03-17 11:55:01
  * @FilePath: /electron-client/app/components/TeamPayModal/components/Header/index.tsx
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
-import React, { useContext, useState } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import { ReactComponent as WechatIcon } from '../../../../assets/payImg/wechat_icon.svg'
-import { ReactComponent as AlipayIcon } from '../../../../assets/payImg/alipay_icon.svg'
+// import { ReactComponent as AlipayIcon } from '../../../../assets/payImg/alipay_icon.svg'
 import { Modal } from 'antd'
 import { ReactComponent as Close } from '../../../../assets/payImg/close.svg'
-
+import QRCode from 'qrcode'
 import style from './index.module.scss'
 import Protocol from './components/Protocol'
 import { SelectMemberContext } from '../../context/context'
 import SuccessPay from './components/SuccessPay'
+import { useMemoizedFn } from '@flyele/flyele-components'
 
 const PayQrCode = () => {
   const service = useContext(SelectMemberContext)
   const [showSuccess, setShowSuccess] = useState<boolean>(false)
+  const [qrCode, setQrCode] = useState('')
+  useEffect(() => {
+    qrCodeFunction()
+  }, [])
+  //获取二维码
+  const qrCodeFunction = useMemoizedFn(async () => {
+    try {
+      // const { date, dateText } = await getValidityTime()
+      // const currentTime = Math.trunc(new Date().getTime() / 1000) // 当前时间 单位：秒
 
+      // setValidityTime(dateText)
+
+      // url += `&expire_at=${date}&dispatch_at=${currentTime}`
+     const url = 'https://feixiang.cn?a=1'
+      const res = await QRCode.toDataURL(
+        'https://open.weixin.qq.com/connect/oauth2/authorize?appid=wx2edc8ed2729bdddf&redirect_uri=' +
+          encodeURIComponent(url) +
+          '&response_type=code&scope=snsapi_base&state=123&connect_redirect=1#wechat_redirect'
+      )
+//https://open.weixin.qq.com/connect/oauth2/authorize?appid=wx2edc8ed2729bdddf&redirect_uri=http://127.0.0.1:4200/payDetail&response_type=code&scope=snsapi_base&state=123&connect_redirect=1#wechat_redirect
+      setQrCode(res)
+
+    } catch{
+      console.log('00');
+      
+    }
+  })
   return (
     <Modal
       open
@@ -59,18 +86,14 @@ const PayQrCode = () => {
                   setShowSuccess(true)
                 }}
               >
-                <img alt="qrcode" src="" className={style.qrcode} />
+                <img alt="qrcode" src={qrCode} className={style.qrcode} />
               </div>
               <div className={style.payIcon}>
                 <div className={style.iconItem}>
                   <WechatIcon className={style.qrcode}></WechatIcon>
                   <span>微信支付</span>
                 </div>
-                <span>/</span>
-                <div className={style.iconItem}>
-                  <AlipayIcon className={style.qrcode}></AlipayIcon>
-                  <span>支付宝支付</span>
-                </div>
+    
               </div>
             </div>
             {/* 协议 */}
--- libs/service-module/src/lib/PersonPayModal/components/PersonVip/components/RightBlock/index.tsx
+++ libs/service-module/src/lib/PersonPayModal/components/PersonVip/components/RightBlock/index.tsx
@@ -2,7 +2,7 @@
  * @Author: wanghui wanghui@flyele.net
  * @Date: 2023-03-08 09:43:55
  * @LastEditors: wanghui wanghui@flyele.net
- * @LastEditTime: 2023-03-13 19:30:34
+ * @LastEditTime: 2023-03-15 15:23:27
  * @FilePath: /electron-client/app/components/PersonPayModal/components/PersonVip/components/RightBlock/index.tsx
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -62,7 +62,7 @@ const RightBlock = () => {
 
   return (
     <div className={style.rightBlock}>
-      {false && (
+      { (
         <div className={style.mealBlock}>
           <div className={style.title}>套餐选择</div>
           <div className={style.mealList}>
@@ -99,14 +99,14 @@ const RightBlock = () => {
         </div>
       )}
       {/* 支付按钮 */}
-      {false && (
+      {(
         <div>
           <PayButton vipMealType={VipMealType.PERSON} />
         </div>
       )}
-      <div>
+    {  false&&<div>
         <PersonVipEmpty></PersonVipEmpty>
-      </div>
+      </div>}
     </div>
   )
 }

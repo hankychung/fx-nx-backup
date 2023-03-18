d4fccca1600b023192e4c05d6733cbdc51515b56
Han <zhonghanjie@flyele.net>
2023-03-18 18:44:51 +0800

    chore: payment api

--- libs/service-module/src/index.ts
+++ libs/service-module/src/index.ts
@@ -5,7 +5,8 @@ import { QuickPay } from './lib/QuickPay/index' //快捷支付弹窗
 import PersonPayModal from './lib/PersonPayModal/index' //个人支付弹窗
 import TeamPayModal from './lib/TeamPayModal/index' //团队支付
 import PayModal from './lib/PayModal/index' //支付聚合
-import CustomerServicesModal from './lib/CustomerServicesModal/index' //客服弹窗
+import CustomerServicesModal from './lib/CustomerServicesModal/index'
+import { init } from '@flyele-nx/api' //客服弹窗
 export * from './lib/service-module'
 export {
   MemberIntroduction,
@@ -17,3 +18,5 @@ export {
   PayModal,
   CustomerServicesModal
 }
+
+export const registerPaymentInit = init

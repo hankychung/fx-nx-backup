bab4fc9486ba5a86360103046456520a6d185b5a
wanghui <wanghui@flyele.net>
2023-03-21 15:15:11 +0800

    feat: 处理token

--- libs/service-module/src/lib/TeamPayModal/components/PayQrCode/index.tsx
+++ libs/service-module/src/lib/TeamPayModal/components/PayQrCode/index.tsx
@@ -2,7 +2,7 @@
  * @Author: wanghui wanghui@flyele.net
  * @Date: 2023-03-09 09:55:49
  * @LastEditors: wanghui wanghui@flyele.net
- * @LastEditTime: 2023-03-20 20:16:20
+ * @LastEditTime: 2023-03-21 15:12:44
  * @FilePath: /electron-client/app/components/TeamPayModal/components/Header/index.tsx
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -85,7 +85,11 @@ const PayQrCode = ({
             <div className={style.payInfo}>
               <div className={style.price}>
                 <span> ￥</span>
-                <span>{regFenToYuan((payInfo && payInfo.now_price) || 0)}</span>
+                <span>
+                  {regFenToYuan(
+                    (payInfo && payInfo.now_price * userInfo.length) || 0
+                  )}
+                </span>
               </div>
               <div
                 className={style.code}

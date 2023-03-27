513c28f90ce9c5b34f42268b8649711e4924debd
wanghui <wanghui@flyele.net>
2023-03-27 17:14:52 +0800

    feat: 协作人状态

--- libs/service-module/src/lib/team-pay-modal/components/team-vip/components/left-block/index.tsx
+++ libs/service-module/src/lib/team-pay-modal/components/team-vip/components/left-block/index.tsx
@@ -2,7 +2,7 @@
  * @Author: wanghui wanghui@flyele.net
  * @Date: 2023-03-07 20:52:57
  * @LastEditors: wanghui wanghui@flyele.net
- * @LastEditTime: 2023-03-27 16:03:48
+ * @LastEditTime: 2023-03-27 17:14:25
  * @FilePath: /electron-client/app/components/PersonPayModal/components/TeamVip/components/LeftBlock/index.tsx
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -42,6 +42,7 @@ const LeftBlock = (props: Iprops) => {
   const setMemberSet = useMemoizedFn(() => {
     if (vipType !== VipPayType.NOVIPCREATE) {
       setResultArr([...memberList])
+      service.selectMember({list:[...memberList]})
     }
   })
   useEffect(() => {

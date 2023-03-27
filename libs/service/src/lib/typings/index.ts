37c1f6e8bb195b8660f9c68135e3fb45e2973e31
Han <zhonghanjie@flyele.net>
2023-03-27 16:34:15 +0800

    style: format

--- libs/service/src/lib/typings/index.ts
+++ libs/service/src/lib/typings/index.ts
@@ -1,4 +1,3 @@
-
 /**
  * 获取对象某个key的值类型
  */
@@ -57,14 +56,13 @@ export interface ICreateOrderParams {
   total_price: number
   users_id: string[]
 }
-//获取支付信息     
+//获取支付信息
 export interface IGetPayParams {
   code: string
   name: string
   order_method: number //下单的途径，0->默认未知，1->APP下单，2->小程序下单，3->JSAPI下单，4->Native下单，5->H5下单，6->支付宝下单
   out_trade_no: string
 }
-
 
 /**
  * 登录相关的如下

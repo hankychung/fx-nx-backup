a316404a3c882b28b3ce71aea457b33df8c4b738
Han <zhonghanjie@flyele.net>
2023-04-06 10:14:05 +0800

    chore: utils for sw

--- libs/sw-sql/src/index.ts
+++ libs/sw-sql/src/index.ts
@@ -3,16 +3,28 @@ import { PostData, ServiceWorkerKey } from '@flyele-nx/sw-sql-client'
 
 class DBHandler {
   constructor() {
-    self.onmessage = ({ data }: MessageEvent<PostData>) => {
+    self.onmessage = async ({ data }: MessageEvent<PostData>) => {
       console.log('from client', data, data.data)
+
+      let responseData: any = null
 
       switch (data.key) {
         case ServiceWorkerKey.INIT_DB: {
-          sqlStore.initDB(data.data as any).then(() => {
-            self.postMessage({ uid: data.uid })
-          })
+          await sqlStore.initDB(data.data as any)
+
+          break
+        }
+
+        case ServiceWorkerKey.UPDATE_TOKEN: {
+          sqlStore.updateToken(data.data as string)
+
+          responseData = undefined
+
+          break
         }
       }
+
+      self.postMessage({ uid: data.uid, data: responseData })
     }
   }
 }

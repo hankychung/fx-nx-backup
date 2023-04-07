dc2fffca9cb3be0dc93e168298ae86b875c19c60
wanghui <wanghui@flyele.net>
2023-04-07 14:42:30 +0800

    feat: 添加调试工具

--- apps/h5/src/app/pages/pay-detail/index.tsx
+++ apps/h5/src/app/pages/pay-detail/index.tsx
@@ -125,6 +125,8 @@ const PayDetail = () => {
         paySign: params.pay_sign //微信签名
       },
       function (res: any) {
+        console.log(res,"测试");
+        
         if (res.err_msg === 'get_brand_wcpay_request:ok') {
           // 使用以上方式判断前端返回,微信团队郑重提示：
           // res.err_msg将在用户支付成功后返回ok，但并不保证它绝对可靠。

e5b13f5f8ff78d22e891ac17cedba52eb0b3ad22
wanghui <wanghui@flyele.net>
2023-04-07 11:25:13 +0800

    feat: 修改域名

--- apps/h5/src/main.tsx
+++ apps/h5/src/main.tsx
@@ -1,8 +1,11 @@
+import { envStore } from '@flyele-nx/service'
 import { StrictMode } from 'react'
 import * as ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 
 import App from './app/app'
+
+envStore.initEnv(process.env.NODE_ENV as string)
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(

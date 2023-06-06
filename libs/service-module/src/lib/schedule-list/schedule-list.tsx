6219b7ca76dee98ebd6da5fd254bfaa08ff7fb44
Han <zhonghanjie@flyele.net>
2023-06-06 15:54:59 +0800

    chore: 事项/子事项数据流

--- libs/service-module/src/lib/schedule-list/schedule-list.tsx
+++ libs/service-module/src/lib/schedule-list/schedule-list.tsx
@@ -4,32 +4,49 @@ import styles from './schedule-list.module.scss'
 import { useMemoizedFn } from 'ahooks'
 import { useScheduleStore } from './utils/useScheduleStore'
 import { ScheduleTask } from './components/schedule-task'
-import { getKey } from './utils'
 
 interface ScheduleListProps {
   date: string
 }
 
 const _ScheduleList: React.FC<ScheduleListProps> = ({ date }) => {
-  const initDate = useScheduleStore((state) => state.initDate)
+  const initList = useScheduleStore((state) => state.initList)
+  const updateTask = useScheduleStore((state) => state.updateTask)
+
   const list = useScheduleStore((state) => state.schedule[date])
 
-  const initList = useMemoizedFn(() => {
+  const fetchList = useMemoizedFn(() => {
     BizApi.getScheduleList({ type: 'today', day: date }).then((res) => {
       console.log('bizres', res)
 
-      initDate({ date, list: res.data?.schedule || [] })
+      const list = res.data?.schedule || []
+
+      const keys: string[] = []
+
+      list.forEach((item) => {
+        const { ref_task_id, repeat_id, finish_time } = item
+
+        // 循环事项且已经完成, 以taskId + repeatId为key
+        const key =
+          repeat_id && finish_time ? `${ref_task_id}-${repeat_id}` : ref_task_id
+
+        updateTask({ key, task: item })
+
+        keys.push(key)
+      })
+
+      initList({ date, list: keys })
     })
   })
 
   return (
     <div className={styles['container']}>
-      <span onClick={initList}>init</span>
+      <span onClick={fetchList}>init</span>
       <span>{date}</span>
 
       <div>
         {(list || []).map((i) => (
-          <ScheduleTask data={i} date={date} key={getKey(i)} />
+          <ScheduleTask date={date} key={i} taskKey={i} />
         ))}
       </div>
     </div>
--- libs/service-module/src/lib/schedule-list/utils/useScheduleStore.ts
+++ libs/service-module/src/lib/schedule-list/utils/useScheduleStore.ts
@@ -3,22 +3,100 @@ import { IScheduleTask } from '@flyele-nx/service'
 import { produce } from 'immer'
 
 interface IState {
-  schedule: { [k: string]: IScheduleTask[] }
+  // 以下所有日期共用
+  taskDict: { [k: string]: IScheduleTask }
+  childrenDict: { [k: string]: string[] }
+  // 以下与日期相关, 每个日期独立维护
+  schedule: { [k: string]: string[] }
+  expandedDict: {
+    [k: string]: {
+      [k: string]: boolean
+    }
+  }
 }
 
-const useScheduleStore = create<
-  IState & {
-    initDate: (options: { date: string; list: IScheduleTask[] }) => void
-  }
->((set) => {
+interface IMutation {
+  initList: (options: { date: string; list: string[] }) => void
+  updateTask: (info: { key: string; task: IScheduleTask }) => void
+  updateExpandedDict: (info: {
+    date: string
+    taskId: string
+    isExpanded: boolean
+  }) => void
+  updateChildDict: (info: { parentKey: string; childrenIds: string[] }) => void
+  batchUpdateChildDict: (info: { [k: string]: string[] }) => void
+}
+
+const useScheduleStore = create<IState & IMutation>((set) => {
   return {
+    /**
+     * 日程列表字典, key为日期, value为该日期下事项列表id数组(带排序)
+     */
     schedule: {},
-    initDate({ date, list }) {
+    /**
+     * 子事项字典
+     */
+    childrenDict: {},
+    /**
+     * 事项字典, key为事项id或事项id+repeadId, value为事项信息
+     */
+    taskDict: {},
+    /**
+     * 事项展开字典, key为日期, value为该日期下的事项收合字典
+     */
+    expandedDict: {},
+    /**
+     * 初始化/重置事项列表
+     */
+    initList({ date, list }) {
       set(
         produce((state: IState) => {
           state.schedule[date] = list
         })
       )
+    },
+    /**
+     * 更新事项
+     */
+    updateTask({ key, task }) {
+      set(
+        produce((state: IState) => {
+          state.taskDict[key] = task
+        })
+      )
+    },
+    /**
+     * 更新对应日期下的事项展开字典
+     */
+    updateExpandedDict({ date, taskId, isExpanded }) {
+      set(
+        produce((state: IState) => {
+          if (!state.expandedDict[date]) state.expandedDict[date] = {}
+
+          state.expandedDict[date][taskId] = isExpanded
+        })
+      )
+    },
+    /**
+     * 更新直属下级事项字典
+     */
+    updateChildDict({ parentKey, childrenIds }) {
+      set(
+        produce((state: IState) => {
+          state.childrenDict[parentKey] = childrenIds
+        })
+      )
+    },
+    /**
+     * 批量更新直属下级事项字典
+     */
+    batchUpdateChildDict(info) {
+      set((state) => ({
+        childrenDict: {
+          ...state.childrenDict,
+          ...info
+        }
+      }))
     }
   }
 })

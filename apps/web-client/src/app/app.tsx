06ee3b694eeae81e98654cd9b3113f89df6345d9
Han <zhonghanjie@flyele.net>
2023-06-16 14:39:41 +0800

    chore: 请求超时限制

--- apps/web-client/src/app/app.tsx
+++ apps/web-client/src/app/app.tsx
@@ -4,13 +4,13 @@ import { ScheduleList } from '@flyele-nx/service-module'
 envStore.initEnv(process.env.NODE_ENV as string)
 
 service.updateToken(
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE2ODY3NDA4NDQsImlhdCI6MTY4NjczMzA1NSwiaXNzIjoiYXBpLmZseWVsZS5uZXQiLCJVc2VySUQiOiIxMzMxMzYzMzU0NTA5NDgyIiwiRGV2aWNlSUQiOiJjMWE1MTcwYy1hYTEyLTQxZWItODQyMC1kMTZkNTA4NWNlYzEiLCJQbGF0Zm9ybSI6Im1vYmlsZSIsIkNsaWVudFZlcnNpb24iOiIyLjMwLjEwIiwiUGhvbmUiOiIiLCJOaWNrTmFtZSI6IiIsIkF2YXRhciI6IiJ9.J7aSY4TUTTHGjmcxhKYKyXut6-JnYkT2mI0R6Q44__s'
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE2ODY5MDM2NjksImlhdCI6MTY4Njg5NTc5NywiaXNzIjoiYXBpLmZseWVsZS5uZXQiLCJVc2VySUQiOiIxMzMxMzYzMzU0NTA5NDgyIiwiRGV2aWNlSUQiOiJhZDIyNzBhYS05NTQ2LTRjZjgtYTY3NC0zZmY0MjJjNWQ3YzIiLCJQbGF0Zm9ybSI6Im1vYmlsZSIsIkNsaWVudFZlcnNpb24iOiIyLjMwLjEwIiwiUGhvbmUiOiIiLCJOaWNrTmFtZSI6IiIsIkF2YXRhciI6IiJ9.dj6I7E0CzX1cPCIy1DLIgrRGknKvrqDMoEHqhorfy_I'
 )
 
 export function App() {
   return (
     <>
-      <ScheduleList date="2023-06-14" />
+      <ScheduleList date="2023-06-16" />
       {/* <ScheduleList date="2023-06-02" />
       <ScheduleList date="2023-06-03" /> */}
     </>

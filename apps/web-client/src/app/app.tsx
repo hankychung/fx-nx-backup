2b86b8374a7c9a4e02def0abe7469f33d13919db
朱廷谦 <zhutingqian@flyele.net>
2023-06-19 14:34:11 +0800

    fix: dayjs的add不会改变原变量，导致循环事项陷入死循环

--- apps/web-client/src/app/app.tsx
+++ apps/web-client/src/app/app.tsx
@@ -4,11 +4,11 @@ import { ScheduleList } from '@flyele-nx/service-module'
 envStore.initEnv(process.env.NODE_ENV as string)
 
 service.updateToken(
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE2ODY5MTg1NDAsImlhdCI6MTY4NjkxMDI2NSwiaXNzIjoiYXBpLmZseWVsZS5uZXQiLCJVc2VySUQiOiIxNjU3MjM5MjkxMDM1Nzc3IiwiRGV2aWNlSUQiOiJjOGM5MWY1Zi1hYWRhLTRkNjMtOGIxMC1iM2JkMTk3MWYzMTAiLCJQbGF0Zm9ybSI6Im1vYmlsZSIsIkNsaWVudFZlcnNpb24iOiIyLjMwLjEwIiwiUGhvbmUiOiIiLCJOaWNrTmFtZSI6IiIsIkF2YXRhciI6IiJ9.9YXhbrrEMTfod-gK2fjxXqmcc3FsjUmmISiAIhXzZZo'
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE2ODcxNjI2NjksImlhdCI6MTY4NzE1NTQxMiwiaXNzIjoiYXBpLmZseWVsZS5uZXQiLCJVc2VySUQiOiIxMzMxMzYzMzU0NTA5NDgyIiwiRGV2aWNlSUQiOiI1MGNiZDIyNy0zOWE4LTQ1MTctYmViNy0xN2I5MzRmNWJmMGIiLCJQbGF0Zm9ybSI6Im1vYmlsZSIsIkNsaWVudFZlcnNpb24iOiIyLjMwLjEwIiwiUGhvbmUiOiIiLCJOaWNrTmFtZSI6IiIsIkF2YXRhciI6IiJ9.Ff_0qg5fNRjDfHYHpSsl2-PMmpPAvFf-mEX8Wc530GE'
 )
 
 export function App() {
-  return <ScheduleList date="2023-06-16" />
+  return <ScheduleList date="2023-06-19" />
 }
 
 export default App

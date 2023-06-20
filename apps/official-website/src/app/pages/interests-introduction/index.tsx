1c4163545e986a01b48ca100baca9e69a240c61f
雷嘉成 <leijiacheng@flyele.net>
2023-06-20 20:59:41 +0800

    fix: 权益部分样式修改

--- apps/official-website/src/app/pages/interests-introduction/index.tsx
+++ apps/official-website/src/app/pages/interests-introduction/index.tsx
@@ -10,6 +10,8 @@ import { useMount } from 'ahooks'
 const InterestsIntroduction = () => {
   // 在移动端ui没出来之前，暂时直接把宽度拉大 临时方案
   const [widthStyle, setWidthStyle] = useState('100%')
+  // 权益身份对比切换状态true为身份权益对比,false为空间权益对比
+  const [showComparison, setShowComparison] = useState(true)
 
   const goToPage = () => {
     window.location.replace('https://www.feixiang.cn/bbxz')
@@ -24,8 +26,33 @@ const InterestsIntroduction = () => {
   return (
     <div className={styles.introductionPage}>
       <MemberIntroduction widthStyle={widthStyle} />
-      <IdentityEqualComparison />
-      <SpaceEqualComparison />
+      <div className={styles.ComparsionTitle}>
+        <div
+          // className={styles.useTitle}
+          onClick={() => {
+            setShowComparison(true)
+          }}
+        >
+          <span
+            className={showComparison ? styles.useTitle : styles.unUseTitle}
+          >
+            身份权益对比
+          </span>
+        </div>
+        <div
+          // className={styles.useTitle}
+          onClick={() => {
+            setShowComparison(false)
+          }}
+        >
+          <span
+            className={showComparison ? styles.unUseTitle : styles.useTitle}
+          >
+            空间权益对比
+          </span>
+        </div>
+      </div>
+      {showComparison ? <IdentityEqualComparison /> : <SpaceEqualComparison />}
 
       <div className={styles.useBoxRoot} style={{ width: widthStyle }}>
         <div className={styles.useTitle}>立即开始管理你的事项</div>

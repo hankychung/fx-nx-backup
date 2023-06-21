3b3b47f6486a50b261094c67f000c423fb3d8b1a
朱廷谦 <zhutingqian@flyele.net>
2023-06-21 10:35:08 +0800

    fix: 卡片内元素会滚动的问题、严格模式下的findDOMNode问题

--- apps/web-client/src/app/app.tsx
+++ apps/web-client/src/app/app.tsx
@@ -9,13 +9,13 @@ import React from 'react'
 envStore.initEnv(process.env.NODE_ENV as string)
 
 service.updateToken(
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE2ODcyNTkzODQsImlhdCI6MTY4NzI1MTg3MiwiaXNzIjoiYXBpLmZseWVsZS5uZXQiLCJVc2VySUQiOiIxMzMxMzYzMzU0NTA5NDgyIiwiRGV2aWNlSUQiOiI5Yjk1NTYzZS0zNWQwLTQ2NDUtODI4Yi03MDNjNDJmNDdkNDYiLCJQbGF0Zm9ybSI6Im1vYmlsZSIsIkNsaWVudFZlcnNpb24iOiIyLjMwLjEwIiwiUGhvbmUiOiIiLCJOaWNrTmFtZSI6IiIsIkF2YXRhciI6IiJ9.aGAjmIt28s1nAZT-QOvoBkr5qT4BTxGUc4urCk4Qg-A'
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE2ODczMjEwNjMsImlhdCI6MTY4NzMxMzQyMCwiaXNzIjoiYXBpLmZseWVsZS5uZXQiLCJVc2VySUQiOiIxNjU3MjM5MjkxMDM1Nzc3IiwiRGV2aWNlSUQiOiI0MGI4MTc1My1hNjg5LTQ5YTItYWU5NC0yOTc3YmM5ODJmYmIiLCJQbGF0Zm9ybSI6Im1vYmlsZSIsIkNsaWVudFZlcnNpb24iOiIyLjMwLjEwIiwiUGhvbmUiOiIiLCJOaWNrTmFtZSI6IiIsIkF2YXRhciI6IiJ9.bcfSmfQoXW1S4fVR4q5QoHTDb0vvsjRyAYCRR3hvuR8'
 )
 
 export function App() {
   return (
     <>
-      <ScheduleList date="2023-06-20" />
+      <ScheduleList date="2023-06-21" />
       <ContextMenu ref={(r) => contextMenuTool.registerContextMenu(r)} />
     </>
   )

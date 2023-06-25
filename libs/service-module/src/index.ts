8f3e4b229612e2dc890bb2eb88f6981b8d759e7f
Han <zhonghanjie@flyele.net>
2023-06-25 09:39:22 +0800

    chore: updateNxToken

--- libs/service-module/src/index.ts
+++ libs/service-module/src/index.ts
@@ -13,6 +13,7 @@ import { init, objectiveApi as NxObjectiveApi } from '@flyele-nx/api' //客服�
 import { AvatarEdit } from './lib/avatar-edit' //头像编辑
 import { ContextMenu } from './lib/context-menu'
 import { contextMenuTool } from './lib/context-menu/contextMenuTool'
+import { service } from '@flyele-nx/service'
 
 export * from './lib/schedule-list/schedule-list' // 日程列表
 
@@ -38,5 +39,7 @@ export * from './lib/map-svg/type/props'
 export * from './lib/global'
 
 export const SMobjectiveApi = NxObjectiveApi
+
+export const updateNxToken = service.updateToken.bind(service)
 
 export const registerPaymentInit = init

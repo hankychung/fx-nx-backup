8581fe60c3ef0052320383fd75e15870d52a72ed
朱廷谦 <zhutingqian@flyele.net>
2023-06-27 16:43:03 +0800

    fix: ts

--- libs/service-module/src/lib/global/types/channel/SIZE_TYPE.ts
+++ libs/service-module/src/lib/global/types/channel/SIZE_TYPE.ts
@@ -6,7 +6,7 @@ export enum SIZE_TYPE_KEY {
   确认弹窗 = 'model-selector',
   邀请协作人 = 'invite_taker',
   多端提醒 = 'reminder_multiend',
-  续费弹窗 = 'pay_modal',
+  续费弹窗 = 'pay_modal'
 }
 
 export interface ISize {
@@ -17,34 +17,34 @@ export interface ISize {
 export const SIZE_CHOICE: { [key: string]: ISize } = {
   [SIZE_TYPE_KEY.时间选择]: {
     w: 375 + 20,
-    h: 663,
+    h: 663
   },
   [SIZE_TYPE_KEY.提醒时间选择]: {
     w: 340, // w: 350,
-    h: 680,
+    h: 680
   },
   [SIZE_TYPE_KEY.会议的时间选择]: {
     w: 500 + 20,
-    h: 663,
+    h: 663
   },
   [SIZE_TYPE_KEY.文件选择器]: {
     w: 590 + 20 + 40,
-    h: 633,
+    h: 633
   },
   [SIZE_TYPE_KEY.确认弹窗]: {
     w: 370 + 20 + 40,
-    h: 633,
+    h: 633
   },
   [SIZE_TYPE_KEY.邀请协作人]: {
     w: 640 + 20,
-    h: 690,
+    h: 690
   },
   [SIZE_TYPE_KEY.多端提醒]: {
     w: 460 + 20,
-    h: 680,
+    h: 680
   },
   [SIZE_TYPE_KEY.续费弹窗]: {
     w: 400,
-    h: 663,
-  },
+    h: 663
+  }
 }

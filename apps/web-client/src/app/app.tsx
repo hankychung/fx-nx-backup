9d97f03411302401fa6e590db8b93d3c1b14bcf8
朱廷谦 <zhutingqian@flyele.net>
2023-06-28 16:54:58 +0800

    fix: 调整右键菜单超出显示区域的问题

--- apps/web-client/src/app/app.tsx
+++ apps/web-client/src/app/app.tsx
@@ -10,7 +10,7 @@ import { useMessage } from '@flyele-nx/ui'
 envStore.initEnv(process.env.NODE_ENV as string)
 
 service.updateToken(
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE2ODc3Nzg5MjcsImlhdCI6MTY4Nzc3MTM4NiwiaXNzIjoiYXBpLmZseWVsZS5uZXQiLCJVc2VySUQiOiI1NDI0MDg0MjE2NzExODUiLCJEZXZpY2VJRCI6ImU2MWQxMmEzLWY2NjYtNDZmMi1hOGY3LWUwZTYwZTQxZGMwNyIsIlBsYXRmb3JtIjoibW9iaWxlIiwiQ2xpZW50VmVyc2lvbiI6IjIuMzAuMTAiLCJQaG9uZSI6IiIsIk5pY2tOYW1lIjoiIiwiQXZhdGFyIjoiIn0.a6uStDg2iFFgp3GdGiB4eIN_V9noRLhWb2C405FO5FY'
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE2ODc5NDc1ODYsImlhdCI6MTY4NzkzOTcxNSwiaXNzIjoiYXBpLmZseWVsZS5uZXQiLCJVc2VySUQiOiIxNjU3MjM5MjkxMDM1Nzc3IiwiRGV2aWNlSUQiOiI0MjBmYjNjNS01ZmY1LTRhNjYtYjNiNi1iOWM0OTkzMTRkNTAiLCJQbGF0Zm9ybSI6Im1vYmlsZSIsIkNsaWVudFZlcnNpb24iOiIyLjMwLjEwIiwiUGhvbmUiOiIiLCJOaWNrTmFtZSI6IiIsIkF2YXRhciI6IiJ9.xB48b57TdYmxwW_5O3XneNRXGtG6rkrvXWx_zT_tp-k'
 )
 
 export function App() {
@@ -19,7 +19,7 @@ export function App() {
   return (
     <>
       {contextHolder}
-      <ScheduleList date="2023-06-26" />
+      <ScheduleList date="2023-06-28" isBoard={true} />
       <ContextMenu ref={(r) => contextMenuTool.registerContextMenu(r)} />
     </>
   )

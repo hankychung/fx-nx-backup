7dee0bc139f35b747a02c0c03869aee7aa6d44db
lhl <laihongliang@flyele.net>
2023-07-03 10:10:35 +0800

    fix: schedule-list loading

--- libs/service-module/src/lib/schedule-list/modules/all-schedule-list.tsx
+++ libs/service-module/src/lib/schedule-list/modules/all-schedule-list.tsx
@@ -163,61 +163,61 @@ const _AllScheduleList: ForwardRefRenderFunction<
 
   return (
     <div className={classNames(styles['container'], overlayClassName)}>
-      {(list || []).length ? (
-        <InfiniteScroll
-          loadMore={fetchList}
-          useWindow={false}
-          hasMore
-          initialLoad={false}
-          className={styles.scroller}
-        >
-          {list.map((i) => (
-            // curTime 应该读取后端的，参考原来的代码 app/utils/timeGetter.ts
-            <ScheduleTask
-              date={date}
-              key={i}
-              taskKey={i}
-              topId={i}
-              curTime={dayjs().unix()}
-              isVipWin={isVipWin}
-              isBoard={isBoard}
+      <InfiniteScroll
+        loadMore={fetchList}
+        useWindow={false}
+        hasMore
+        initialLoad={false}
+        className={styles.scroller}
+      >
+        {(list ?? []).map((i) => (
+          // curTime 应该读取后端的，参考原来的代码 app/utils/timeGetter.ts
+          <ScheduleTask
+            date={date}
+            key={i}
+            taskKey={i}
+            topId={i}
+            curTime={dayjs().unix()}
+            isVipWin={isVipWin}
+            isBoard={isBoard}
+            isDarkMode={isDarkMode}
+          />
+        ))}
+
+        {!!finishTotal && pageFetchFinished ? (
+          <>
+            <FinishNumBtn
+              show={showFinished}
+              count={finishTotal}
               isDarkMode={isDarkMode}
+              onToggleShow={onToggleShowFinished}
             />
-          ))}
-          {!!finishTotal && pageFetchFinished && (
-            <>
-              <FinishNumBtn
-                show={showFinished}
-                count={finishTotal}
-                isDarkMode={isDarkMode}
-                onToggleShow={onToggleShowFinished}
-              />
-              {showFinished &&
-                (finishList || []).map((i) => (
-                  // curTime 应该读取后端的，参考原来的代码 app/utils/timeGetter.ts
-                  <ScheduleTask
-                    date={date}
-                    key={i}
-                    taskKey={i}
-                    topId={i}
-                    curTime={dayjs().unix()}
-                    isVipWin={isVipWin}
-                    isBoard={isBoard}
-                    isDarkMode={isDarkMode}
-                  />
-                ))}
-            </>
-          )}
-        </InfiniteScroll>
-      ) : (
-        <EmptyData
-          isError={isError}
-          listType={isFinished ? 'COMPLETE' : 'NORMAL'}
-          isBoard={!!isBoard}
-          noTask={!finishList?.length && !list?.length}
-          allFinished={!!finishTotal && !list?.length}
-        />
-      )}
+            {showFinished &&
+              (finishList || []).map((i) => (
+                // curTime 应该读取后端的，参考原来的代码 app/utils/timeGetter.ts
+                <ScheduleTask
+                  date={date}
+                  key={i}
+                  taskKey={i}
+                  topId={i}
+                  curTime={dayjs().unix()}
+                  isVipWin={isVipWin}
+                  isBoard={isBoard}
+                  isDarkMode={isDarkMode}
+                />
+              ))}
+          </>
+        ) : (
+          <EmptyData
+            isError={isError}
+            listType={isFinished ? 'COMPLETE' : 'NORMAL'}
+            isBoard={!!isBoard}
+            noTask={!finishList?.length && !list?.length}
+            allFinished={!!finishTotal && !list?.length}
+            loading={loading}
+          />
+        )}
+      </InfiniteScroll>
     </div>
   )
 }

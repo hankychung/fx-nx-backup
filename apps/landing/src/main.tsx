da41b7cd96942c225a03da893c40b365bcdf90b0
Han <zhonghanjie@flyele.net>
2023-07-04 09:49:39 +0800

    test: sensor

--- apps/landing/src/main.tsx
+++ apps/landing/src/main.tsx
@@ -1,8 +1,11 @@
 import { StrictMode } from 'react'
 import * as ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
+import sensor from 'sa-sdk-javascript'
 
 import App from './app/app'
+
+sensor.init({})
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(

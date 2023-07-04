33f086e7d11162135fa9483069de8c5450a2ed2c
Han <zhonghanjie@flyele.net>
2023-07-04 15:16:04 +0800

    chore: updateFinishedCount

--- libs/service-module/src/lib/global/nxController.ts
+++ libs/service-module/src/lib/global/nxController.ts
@@ -133,6 +133,13 @@ class GlobalNxController {
       console.log('handlerTaskAddTaker 未注册')
     }
   }
+
+  /**
+   * 更新今日已完成数量
+   */
+  updateFinishedCount(count: number) {
+    console.log('已完成数量', count)
+  }
 }
 
 export const globalNxController = new GlobalNxController()

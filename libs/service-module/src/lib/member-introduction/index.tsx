82031e1f76e32e212aa9f372c17f02a626eeaa6d
雷嘉成 <leijiacheng@flyele.net>
2023-07-20 17:10:43 +0800

    fix: 个人支付页面价格位置微调4px

--- libs/service-module/src/lib/member-introduction/index.tsx
+++ libs/service-module/src/lib/member-introduction/index.tsx
@@ -49,19 +49,19 @@ export const MemberIntroduction = ({
 
   const onClickBtn = async (key: string) => {
     // 方便调试直接放token可以绕过扫码登录
-    await service.updateToken(
-      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE2ODk3NjI0NzUsImlhdCI6MTY4OTc1NDcwOCwiaXNzIjoiYXBpLmZseWVsZS5uZXQiLCJVc2VySUQiOiIyMzk5NDQ0NjIxMzI5NjM4IiwiRGV2aWNlSUQiOiJkZjVlYzdjYy03YzBmLTQwNTAtOTBiZC1mZGE3OTY3MGNiZjIiLCJQbGF0Zm9ybSI6Im1vYmlsZSIsIkNsaWVudFZlcnNpb24iOiIyLjMwLjEwIiwiUGhvbmUiOiIiLCJOaWNrTmFtZSI6IiIsIkF2YXRhciI6IiJ9.0iIUgwDIAjZujayhOEO7Sf9N_d-9zbFWczNVQl1T_pk'
-    )
-    setVipType(key)
-    await fetchTakerList()
-    setShow(true)
-    // if (key === 'personal' || key === 'team') {
-    //   setVipType(key)
-    //   setShowLoginModal(true)
-    // }
-    // if (key === 'custom') {
-    //   setShowCustomerModal(true)
-    // }
+    // await service.updateToken(
+    //   'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE2ODk3NjI0NzUsImlhdCI6MTY4OTc1NDcwOCwiaXNzIjoiYXBpLmZseWVsZS5uZXQiLCJVc2VySUQiOiIyMzk5NDQ0NjIxMzI5NjM4IiwiRGV2aWNlSUQiOiJkZjVlYzdjYy03YzBmLTQwNTAtOTBiZC1mZGE3OTY3MGNiZjIiLCJQbGF0Zm9ybSI6Im1vYmlsZSIsIkNsaWVudFZlcnNpb24iOiIyLjMwLjEwIiwiUGhvbmUiOiIiLCJOaWNrTmFtZSI6IiIsIkF2YXRhciI6IiJ9.0iIUgwDIAjZujayhOEO7Sf9N_d-9zbFWczNVQl1T_pk'
+    // )
+    // setVipType(key)
+    // await fetchTakerList()
+    // setShow(true)
+    if (key === 'personal' || key === 'team') {
+      setVipType(key)
+      setShowLoginModal(true)
+    }
+    if (key === 'custom') {
+      setShowCustomerModal(true)
+    }
   }
 
   const getOrderDetail = useMemoizedFn(() => {

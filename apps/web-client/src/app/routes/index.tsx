0a596e767e25eb24334e68bf75f4b7cfccfabce5
Han <zhonghanjie@flyele.net>
2023-07-20 16:07:26 +0800

    chore: redirect for /login

--- apps/web-client/src/app/routes/index.tsx
+++ apps/web-client/src/app/routes/index.tsx
@@ -4,6 +4,9 @@ import App from '../app'
 import { Login } from '../pages/login'
 import { Board } from '../pages/board'
 import { RoutePath } from './const'
+import { getToken } from '../utils'
+
+const DefaultPage = () => <Navigate to="/" />
 
 const router = createBrowserRouter([
   {
@@ -11,8 +14,11 @@ const router = createBrowserRouter([
     element: <App />,
     children: [{ path: RoutePath.board, element: <Board /> }]
   },
-  { path: RoutePath.login, element: <Login /> },
-  { path: '*', element: <Navigate to="/" /> }
+  {
+    path: RoutePath.login,
+    element: getToken() ? <DefaultPage /> : <Login />
+  },
+  { path: '*', element: <DefaultPage /> }
 ])
 
 export { router }

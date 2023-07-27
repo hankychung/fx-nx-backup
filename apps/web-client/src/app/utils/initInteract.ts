bbca8a5c79931ebbaa153af53af9a1dbe50fdbde
Han <zhonghanjie@flyele.net>
2023-07-27 17:10:38 +0800

    chore: 屏蔽中台查询子事项功能 & 协作人代理

--- apps/web-client/src/app/utils/initInteract.ts
+++ apps/web-client/src/app/utils/initInteract.ts
@@ -1,24 +1,40 @@
 import { BizApi } from '@flyele-nx/service'
 import { GlobalInfoHandler, IContactDict } from '@flyele-nx/service-module'
+import { getUserInfoSafely } from './getUserInfo'
+
+export const invalidTaker = {
+  avatar: 'https://cdn.flyele.net/resources/default_avatar.png',
+  nick_name: 'invalid',
+  original_name: 'invalid',
+  pinyin: 'invalid',
+  remark: 'invalid',
+  userId: 'invalidId',
+  name: 'invalidId'
+}
 
 const initInteract = () => {
+  const userInfo = getUserInfoSafely()
+
+  if (!userInfo) return
+
   BizApi.getInteracts().then((list) => {
-    console.log('@list', list)
     GlobalInfoHandler.updateInteracts(list)
 
-    const dict = list.reduce<IContactDict>((pre, cur) => {
-      const { user_id } = cur
+    const dict = list.reduce<IContactDict>(
+      (pre, cur) => {
+        const { user_id } = cur
 
-      pre[user_id] = cur
+        pre[user_id] = cur
 
-      return pre
-    }, {})
+        return pre
+      },
+      { [userInfo.user_id]: userInfo }
+    )
 
     GlobalInfoHandler.updateContactDict(
       new Proxy(dict, {
         get(target: IContactDict, p: string) {
-          // TODO: fix here
-          return target[p] || {}
+          return target[p] || invalidTaker
         }
       })
     )

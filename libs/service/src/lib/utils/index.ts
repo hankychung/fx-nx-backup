212ec1d0de9614dd31e6b168094a02e5406ee42f
Han <zhonghanjie@flyele.net>
2023-08-01 10:54:12 +0800

    fix: 切换账号概率性拿不到个别协作人信息

--- libs/service/src/lib/utils/index.ts
+++ libs/service/src/lib/utils/index.ts
@@ -2,10 +2,18 @@ import { VipTypeEnum } from '@flyele-nx/constant'
 import { IInteract } from '@flyele-nx/types'
 
 class VipHandler {
-  static checkVipType({
-    vip_type,
-    vip_next_expired_at
-  }: Pick<IInteract, 'vip_type' | 'vip_next_expired_at'>) {
+  static checkVipType(
+    options: Pick<IInteract, 'vip_type' | 'vip_next_expired_at'>
+  ) {
+    if (!options) {
+      return {
+        isTeamVip: false,
+        isVip: false
+      }
+    }
+
+    const { vip_type, vip_next_expired_at } = options
+
     if (vip_next_expired_at)
       return {
         isTeamVip: true,

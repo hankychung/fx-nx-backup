0174b6c16f332a7ddceea357743b98de7219f1bb
Han <zhonghanjie@flyele.net>
2023-08-02 11:06:47 +0800

    chore: 选择日期刷新列表

--- apps/web-client/src/app/pages/board/modules/day-view/components/schedule/index.tsx
+++ apps/web-client/src/app/pages/board/modules/day-view/components/schedule/index.tsx
@@ -1,16 +1,20 @@
 import { AllScheduleList } from '@flyele-nx/service-module'
-import { timeGetter } from '@flyele-nx/utils'
 import dayjs from 'dayjs'
-import { FC, memo } from 'react'
+import { FC, memo, useContext, useMemo } from 'react'
 import style from './index.module.scss'
+import { SelectDateCtx } from '../../context/select-date'
 
 const _Schedule: FC = () => {
+  const { selectedDate } = useContext(SelectDateCtx)
+
+  const date = useMemo(
+    () => dayjs.unix(selectedDate).format('YYYY-MM-DD'),
+    [selectedDate]
+  )
+
   return (
     <div className={style.schedule}>
-      <AllScheduleList
-        isBoard={true}
-        date={dayjs.unix(timeGetter.getDateRoughly()).format('YYYY-MM-DD')}
-      />
+      <AllScheduleList isBoard={true} date={date} />
     </div>
   )
 }

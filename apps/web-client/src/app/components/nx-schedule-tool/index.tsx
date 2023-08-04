560ac997b0db8f8d2474be2fbd8a9d2d1cb97123
朱廷谦 <zhutingqian@flyele.net>
2023-08-04 18:36:20 +0800

    fix: 完善修复bug

---
+++ apps/web-client/src/app/components/nx-schedule-tool/index.tsx
@@ -0,0 +1,10 @@
+import { ContextMenu, contextMenuTool } from '@flyele-nx/service-module'
+
+
+const _NxScheduleTool = () => {
+  return (
+    <ContextMenu ref={(r) => contextMenuTool.registerContextMenu(r)} />
+  )
+}
+
+export const NxScheduleTool = _NxScheduleTool

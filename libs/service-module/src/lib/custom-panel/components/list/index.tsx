22026e506a2bf55345f980e89e3814a22689564c
lhl <laihongliang@flyele.net>
2023-08-16 15:59:18 +0800

    fix: 自定义视图 loading

--- libs/service-module/src/lib/custom-panel/components/list/index.tsx
+++ libs/service-module/src/lib/custom-panel/components/list/index.tsx
@@ -5,6 +5,7 @@ import { ICustomDashboardItem } from '@flyele-nx/types'
 import { useMemo, useRef } from 'react'
 import style from './index.module.scss'
 import ListEmpty from '../list-empty'
+import { Loading } from '@flyele-nx/ui'
 
 interface IProps {
   tabId: string
@@ -13,7 +14,7 @@ interface IProps {
 export const List = (props: IProps) => {
   const { tabId } = props
   const containerRef = useRef<HTMLDivElement>(null)
-  const { data, reloadAsync: reload } = useInfiniteScroll<{
+  const { data, loading } = useInfiniteScroll<{
     list: ICustomDashboardItem[]
     hasNextPage: boolean
     page: number
@@ -46,21 +47,26 @@ export const List = (props: IProps) => {
   const list = useMemo(() => {
     return data?.list ?? []
   }, [data])
+
+  const buildContent = () => {
+    if (loading) return <Loading />
+    else if (list.length) {
+      return list.map((item) => (
+        <Item
+          item={item}
+          key={item.task_id}
+          complete={function (): void {
+            throw new Error('Function not implemented.')
+          }}
+        />
+      ))
+    } else {
+      return <ListEmpty />
+    }
+  }
   return (
     <div ref={containerRef} className={style.list}>
-      {list.length ? (
-        list.map((item) => (
-          <Item
-            item={item}
-            key={item.task_id}
-            complete={function (): void {
-              throw new Error('Function not implemented.')
-            }}
-          />
-        ))
-      ) : (
-        <ListEmpty />
-      )}
+      {buildContent()}
     </div>
   )
 }

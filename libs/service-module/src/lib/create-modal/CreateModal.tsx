afdb87a8bc27cab24268ae1daa53fc3ba191dfcc
lhl <laihongliang@flyele.net>
2023-08-18 14:56:41 +0800

    feat: create modal header

--- libs/service-module/src/lib/create-modal/CreateModal.tsx
+++ libs/service-module/src/lib/create-modal/CreateModal.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import style from './createModal.module.scss'
+import { ModalHeader } from './components/header'
 
 interface IProps {
   close: () => void
@@ -8,8 +9,9 @@ interface IProps {
 const CreateModal: React.FC<IProps> = ({ close }) => {
   return (
     <div className={style['create-modal']}>
-      <div className={style.header}>HEADER</div>
-      <div onClick={close}>close</div>
+      <ModalHeader close={close} />
+      <div className={style.content}>content</div>
+      <div className={style.footer}>footer</div>
     </div>
   )
 }

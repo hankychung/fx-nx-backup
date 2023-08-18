71c9c6af0ce130e58e5c80d768d24243a3714374
lhl <laihongliang@flyele.net>
2023-08-18 16:48:33 +0800

    feat: 创建弹窗 - 标题

--- libs/service-module/src/lib/create-modal/CreateModal.tsx
+++ libs/service-module/src/lib/create-modal/CreateModal.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import style from './createModal.module.scss'
 import { ModalHeader } from './components/header'
+import { TitleInput } from './components/title-input'
 
 interface IProps {
   close: () => void
@@ -10,7 +11,9 @@ const CreateModal: React.FC<IProps> = ({ close }) => {
   return (
     <div className={style['create-modal']}>
       <ModalHeader close={close} />
-      <div className={style.content}>content</div>
+      <div className={style.content}>
+        <TitleInput placeholder="事项标题" />
+      </div>
       <div className={style.footer}>footer</div>
     </div>
   )
---
+++ libs/service-module/src/lib/create-modal/components/title-input/index.tsx
@@ -0,0 +1,80 @@
+import ContentEditable, { ContentEditableEvent } from 'react-contenteditable'
+import style from './index.module.scss'
+import { useState } from 'react'
+import { useMemoizedFn } from 'ahooks'
+import { debounce } from 'lodash'
+import { message } from 'antd'
+import { globalNxController } from '@flyele-nx/global-processor'
+
+interface IProps {
+  inputStyle?: React.CSSProperties
+  maxLen?: number
+  placeholder?: string
+  onEnter?: () => void
+}
+
+export const TitleInput = (props: IProps) => {
+  const {
+    maxLen = 300,
+    inputStyle,
+    placeholder = '请输入标题',
+    onEnter
+  } = props
+  const [value, setValue] = useState('')
+  const [msg, contextHolder] = message.useMessage()
+
+  const debounceOnEnter = useMemoizedFn(
+    debounce(() => {
+      onEnter?.()
+    })
+  )
+
+  // 按下回车
+  const onKeyPress = useMemoizedFn((e) => {
+    if (e.charCode === 13) e.preventDefault()
+
+    if (e.key.toLowerCase() === 'enter' && !e.altKey && !e.ctrlKey) {
+      if (!e.shiftKey) {
+        debounceOnEnter()
+      } else {
+        console.log(
+          'shift+enter 不响应换行，因为目前事项标题不支持换行，后续支持的话 可以搜索这句话修改'
+        )
+      }
+    }
+  })
+
+  const onChange = (e: ContentEditableEvent) => {
+    if (value.length >= maxLen) {
+      // TODO 换showMsg
+      msg.error(`最多支持${maxLen}个字符`)
+      globalNxController.showMsg({
+        msgType: '消息',
+        content: `最多支持${maxLen}个字符`
+      })
+    }
+
+    setValue(e.target.value)
+  }
+  return (
+    <div className={style.container}>
+      <ContentEditable
+        aria-valuemax={maxLen}
+        style={inputStyle}
+        className={style.input}
+        aria-required
+        html={value}
+        disabled={false}
+        onChange={onChange}
+        onKeyPress={onKeyPress}
+      />
+      {!value.length && (
+        <div className={style.placeholder}>
+          {placeholder}
+          <span style={{ color: 'red' }}>*</span>
+        </div>
+      )}
+      {contextHolder}
+    </div>
+  )
+}

45087f9fb8ab5e1531ae6cc6f367b6d562308b2c
Han <zhonghanjie@flyele.net>
2023-08-23 11:29:52 +0800

    chore: inviteModal

--- libs/service-module/src/lib/invite-modal/InviteModal.tsx
+++ libs/service-module/src/lib/invite-modal/InviteModal.tsx
@@ -3,12 +3,17 @@ import { FlyMemberSelector } from '@flyele/flyele-components'
 import style from './inviteModal.module.scss'
 import { useContactStore } from '@flyele-nx/zustand-store'
 import { invitePlugin } from '.'
+import { useMemoizedFn } from 'ahooks'
 
-interface IProps {
+interface IOuterProps {
+  defaultTakers?: string[]
+}
+
+interface IProps extends IOuterProps {
   close: () => void
 }
 
-const InviteModal: React.FC<IProps> = ({ close }) => {
+const InviteModal: React.FC<IProps> = ({ close, defaultTakers = [] }) => {
   const plugins = useMemo(() => {
     return [
       new invitePlugin.interact({
@@ -21,14 +26,26 @@ const InviteModal: React.FC<IProps> = ({ close }) => {
     ]
   }, [])
 
+  const confirm = useMemoizedFn((takerIds: string[]) => {
+    console.log(takerIds)
+    close()
+  })
+
   return (
     <div className={style['invite-modal']}>
       <div className={style.header}>header</div>
       <div>
-        <FlyMemberSelector plugins={plugins} />
+        <FlyMemberSelector
+          plugins={plugins}
+          onConfirm={confirm}
+          onCancel={close}
+          defaultTakers={defaultTakers}
+        />
       </div>
     </div>
   )
 }
 
 export { InviteModal }
+
+export type { IOuterProps }

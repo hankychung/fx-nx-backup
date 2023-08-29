63724be69eb8edaf9bf712974846f61790829adb
lhl <laihongliang@flyele.net>
2023-08-29 14:26:34 +0800

    fix: gift icon

--- libs/icon/src/lib/img.tsx
+++ libs/icon/src/lib/img.tsx
@@ -1,4 +1,5 @@
 import CircleArrowUpImg from './img/circle_arrow_up.svg'
+import VipGift from './img/gift.svg'
 
 type IImg = React.DetailedHTMLProps<
   React.ImgHTMLAttributes<HTMLImageElement>,
@@ -7,4 +8,8 @@ type IImg = React.DetailedHTMLProps<
 
 export const CircleArrowUp: React.FC<IImg> = (props) => {
   return <img {...props} src={CircleArrowUpImg} alt="" />
+}
+
+export const VipGiftIcon: React.FC<IImg> = (props) => {
+  return <img {...props} src={VipGift} alt="" />
 }

7fe2cd0044b99f2b56c82f924c7d798a281a1d44
Han <zhonghanjie@flyele.net>
2023-08-29 15:26:37 +0800

    chore: 注释

--- libs/types/src/zustand.ts
+++ libs/types/src/zustand.ts
@@ -1,11 +1,13 @@
 interface IUploadFile {
   status: 'pending' | 'success' | 'error'
+  // 用于维护文件字典, 仅前端使用
   fileId: string
   name: string
   progress: number
   abortController: AbortController
   file: File
   uploadDictId: string
+  // 真实文件id, 传给后端使用, status为success时有值
   uploadedFileId?: string
 }
 

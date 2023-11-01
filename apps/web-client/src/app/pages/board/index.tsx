8ae953b6dfe244a8644bddc943a1f7934fd5e04f
lhl <laihongliang@flyele.net>
2023-11-01 09:35:07 +0800

    fix: remove impoort

--- apps/web-client/src/app/pages/board/index.tsx
+++ apps/web-client/src/app/pages/board/index.tsx
@@ -3,7 +3,7 @@ import { initCacheWorker } from '../../utils/initCacheWorker'
 import { BoardHeader } from './components/header'
 import { Outlet } from 'react-router-dom'
 import style from './index.module.scss'
-import { TeamPayModal, useUserInfoStore } from '@flyele-nx/service-module'
+import { useUserInfoStore } from '@flyele-nx/service-module'
 import { LocalStore } from '@flyele-nx/utils'
 import { initInteract } from '../../utils/initInteract'
 

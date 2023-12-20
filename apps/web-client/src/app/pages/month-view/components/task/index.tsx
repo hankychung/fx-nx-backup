bb424209ed58c52b54fb69f49a7041ec6a4d728d
Han <zhonghanjie@flyele.net>
2023-12-20 15:24:07 +0800

    chore: makeDraggingItem

--- apps/web-client/src/app/pages/month-view/components/task/index.tsx
+++ apps/web-client/src/app/pages/month-view/components/task/index.tsx
@@ -1,7 +1,14 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import style from './index.module.scss'
+import { makeDraggingItem } from '../../utils/makeDraggingItem'
 
 const Task: React.FC<{ title: string }> = ({ title }) => {
+  const [draggingImg, setDraggingImg] = useState<HTMLImageElement>()
+
+  useEffect(() => {
+    setDraggingImg(makeDraggingItem({ title }))
+  }, [title])
+
   return (
     <div
       className={style['task-item']}
@@ -11,26 +18,8 @@ const Task: React.FC<{ title: string }> = ({ title }) => {
         event.dataTransfer.effectAllowed = 'move'
         event.dataTransfer.dropEffect = 'move'
 
-        // 创建并配置 Canvas 元素
-        const canvas: HTMLCanvasElement = document.createElement('canvas')
-        canvas.width = 100
-        canvas.height = 100
-
-        // 在 Canvas 上绘制正方形
-        const ctx: CanvasRenderingContext2D | null = canvas.getContext('2d')
-        if (ctx) {
-          ctx.fillStyle = 'red'
-          ctx.fillRect(0, 0, 100, 100)
-        }
-
-        // 将 Canvas 的内容转换为图像
-        const img: HTMLImageElement = new Image()
-        img.src = canvas.toDataURL()
-
-        console.log('check src', img.src)
-
         // 设置拖拽时的图像
-        event.dataTransfer.setDragImage(img, 0, 0) // 图像中心作为拖拽点
+        draggingImg && event.dataTransfer.setDragImage(draggingImg, 0, 10) // 图像中心作为拖拽点
       }}
     >
       {title}
